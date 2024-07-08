4f29fb75329d776118f76b4d1a359670885b9dd4
anasalazmeh <anoosalazmeh@gmail.com>
2024-07-08 15:38:30 +0300

    fix phoneInput

--- src/Components/pageFormConsult/ConsultForm.tsx
+++ src/Components/pageFormConsult/ConsultForm.tsx
@@ -18,6 +18,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useNavigate, useParams } from "react-router-dom";
 import { DataConsultants } from "../../typeData";
 import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 const ConsultForm = () => {
   const { t } = useTranslation();
   const [open, setOpen] = React.useState(false);
